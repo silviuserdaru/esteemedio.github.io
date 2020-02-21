b8d678d26c883bc1e7aa1b17acec049f79980fe0
Albert Volkman <albert.volkman@gmail.com>
2020-02-21 16:33:34 -0500

    Remove gulp-filelog

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var child = require('child_process');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var filelog = require('gulp-filelog');
 var htmlmin = require('gulp-htmlmin');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
@@ -31,8 +30,7 @@ gulp.task('sass', function() {
         ],
       }), cssnano()]))
       */
-    .pipe(gulp.dest('./css'))
-    .pipe(filelog());
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
@@ -47,8 +45,7 @@ gulp.task('js', function() {
   ])
     .pipe(concat('esteemed.js'))
     //.pipe(uglify())
-    .pipe(gulp.dest('js'))
-    .pipe(filelog());
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task('js:watch', function() {
